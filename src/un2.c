2ca5b8101fe2c9e68a51f2c91778c37e165fff33
tiandic <145258834+tiandic@users.noreply.github.com>
2025-08-18 17:01:28 +0800

    Fixed the bug where the 'real' file was not deleted.

--- src/un2.c
+++ src/un2.c
@@ -1,6 +1,20 @@
 #include "def.h"
 #include "reg.h"
 
+bool get_dir(char *path,int path_size){
+    // 获取当前程序的所在目录
+    GetModuleFileNameA(NULL, path, path_size);
+
+    // 提取目录部分
+    for (int i = strlen(path) - 1; i >= 0; i--) {
+        if (path[i] == '\\') {
+            path[i + 1] = '\0'; // 截断到最后一个 '\'
+            break;
+        }
+    }
+    return true;
+}
+
 int main(int argc, char const *argv[])
 {
     remove_reg();
