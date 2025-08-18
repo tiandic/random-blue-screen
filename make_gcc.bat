25878ac2c7b5fc4a6c794abf5d1fe0d448742ad2
tiandic <145258834+tiandic@users.noreply.github.com>
2025-08-18 17:08:32 +0800

    Added the '-O2' compilation option.

--- make_gcc.bat
+++ make_gcc.bat
@@ -1,7 +1,7 @@
 @echo off
 
-gcc -mwindows .\src\main.c -o RandomBlueScreenMain.exe
-gcc -mwindows .\src\blueScreen.c app.res -o blueScreen.exe
-gcc -mwindows .\src\install.c -o install.exe
-gcc -mwindows .\src\un2.c -o un2.exe
+gcc -mwindows -O2 .\src\main.c -o RandomBlueScreenMain.exe
+gcc -mwindows -O2 .\src\blueScreen.c app.res -o blueScreen.exe
+gcc -mwindows -O2 .\src\install.c -o install.exe
+gcc -mwindows -O2 .\src\un2.c -o un2.exe
 gcc .\src\create_real.c -o create_real.exe
