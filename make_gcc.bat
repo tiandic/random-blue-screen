38cfb1521d402a3f7f2ababefc9ebcdcdde31bd1
tiandic <145258834+tiandic@users.noreply.github.com>
2025-08-29 14:53:57 +0800

    Fixed the issue of app.res when compiling with gcc.

--- make_gcc.bat
+++ make_gcc.bat
@@ -1,7 +1,9 @@
 @echo off
 
+windres app.rc -o app_res.o
 gcc -mwindows -O2 .\src\main.c -o RandomBlueScreenMain.exe
-gcc -mwindows -O2 .\src\blueScreen.c app.res -o blueScreen.exe
+gcc -mwindows -O2 .\src\blueScreen.c app_res.o -o blueScreen.exe
 gcc -mwindows -O2 .\src\install.c -o install.exe
 gcc -mwindows -O2 .\src\un2.c -o un2.exe
 gcc .\src\create_real.c -o create_real.exe
+del app_res.o
